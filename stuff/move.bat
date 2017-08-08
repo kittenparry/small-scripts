fadc0f27517f9ae8bdef006af836840747d967cf
Edvin Boul <kittenparry@users.noreply.github.com>
2017-08-08 22:48:39 +0300

    Create move.bat

---
+++ stuff/move.bat
@@ -0,0 +1,44 @@
+REM Creates the folders for my downloads folder
+REM and puts files to their places
+
+MD folder.torrent folder.img folder.webm folder.exe folder.rar folder.video folder.oth
+MOVE *.torrent folder.torrent\
+MOVE *.jpg folder.img\
+MOVE *.png folder.img\
+MOVE *.gif folder.img\
+MOVE *.jpeg folder.img\
+MOVE *.webm folder.webm\
+MOVE *.exe folder.exe\
+MOVE *.msi folder.exe\
+MOVE *.rar folder.rar\
+MOVE *.zip folder.rar\
+MOVE *.7z folder.rar\
+MOVE *.mp4 folder.video\
+MOVE *.avi folder.video\
+MOVE *.flv folder.video\
+MOVE *.ts folder.video\
+MOVE *.mov folder.video\
+MOVE *.wmv folder.video\
+MOVE *.mkv folder.video\
+MOVE *.3gp folder.video\
+MOVE *.m4v folder.video\
+MOVE *.mpg folder.video\
+MOVE *.pdf folder.oth\
+MOVE *.mp3 folder.oth\
+MOVE *.txt folder.oth\
+MOVE *.iso folder.oth\
+MOVE *.docx folder.oth\
+MOVE *.rtf folder.oth\
+MOVE *.svg folder.oth\
+MOVE *.js folder.oth\
+MOVE *.pptx folder.oth\
+MOVE *.doc folder.oth\
+MOVE *.m4a folder.oth\
+MOVE *.html folder.oth\
+MOVE *.css folder.oth\
+MOVE *.ogg folder.oth\
+MOVE *.xlsx folder.oth\
+MOVE *.gz folder.oth\
+MOVE *.azw3 folder.oth\
+MOVE *.mobi folder.oth\
+MOVE *.log folder.oth\
