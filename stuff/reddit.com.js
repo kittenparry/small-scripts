f7cc8bc03739b5a3df2ad3c980b2b2c3bf5118b9
Edvin Boul <kittenparry@users.noreply.github.com>
2017-08-08 22:52:54 +0300

    Create reddit.com.js

---
+++ stuff/reddit.com.js
@@ -0,0 +1,42 @@
+//Custom js to add keyboard shortcuts for reddit.com.
+//n for next page
+//b for previous
+//r for random subreddit
+//t for randnsfw
+window.addEventListener("keydown",checkKeyPressed,false);
+function checkKeyPressed(e){
+	type=e.target.getAttribute("type");
+	tag=e.target.tagName.toLowerCase();
+	if(type!='text'&&tag!='textarea'){
+		switch(e.keyCode){
+			case 66:
+				window.location=document.querySelector('a[rel="nofollow prev"]').href;
+				break;
+			case 78:
+				window.location=document.querySelector('a[rel="nofollow next"]').href;
+				break;
+			case 82: //r
+				//window.location=document.querySelector('a[class="random choice"]').href;
+				window.location='https://www.reddit.com/r/random/';
+				break;
+			case 84: //t
+				//window.location=document.querySelector('a[class="random choice"]').href;
+				//window.location=$('a:contains("randnsfw")').attr('href');
+				window.location='https://www.reddit.com/r/randnsfw/';
+				break;
+			/*case 85: //u
+				$('.up:first').trigger('click');
+				break;
+			case 73: //i
+				$('.upmod:first').trigger('click');
+				break;
+			case 49: //1
+				$('.noncollapsed.comment .expand:eq(0)').not('.child .noncollapsed.comment .expand:eq(0)').trigger('click');
+				break;
+			case 50: //2
+				//$('.comment.collapsed .expand:eq(-1)').trigger('click');
+				break;*/
+			default:
+		}
+	}
+}
