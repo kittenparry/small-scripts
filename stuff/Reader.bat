e979e4ded1283e1af9b37fa5ad22905c190a6155
Edvin Boul <kittenparry@users.noreply.github.com>
2017-08-14 02:42:23 +0300

    Create Reader.bat

---
+++ stuff/Reader.bat
@@ -0,0 +1,8 @@
+REM Creates a list of files in the current dir and subdirs to a text file
+REM which will be placed to the current directory
+
+REM With files (Same with Reader.py, but sorted)
+dir /s /b | sort > manifest_dirs_files.txt
+
+REM Without files
+dir /a:d /s /b | sort > manifest_dirs.txt
