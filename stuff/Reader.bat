9a21ab2f3e1d80cd27c935522c5bdfe6fba3adc8
Edvin Boul <kittenparry@users.noreply.github.com>
2017-08-14 02:55:23 +0300

    Unicode problem
    Can't output in Unicode

--- stuff/Reader.bat
+++ stuff/Reader.bat
@@ -1,5 +1,6 @@
 REM Creates a list of files in the current dir and subdirs to a text file
 REM which will be placed to the current directory
+REM Doesn't output in Unicode, unlike Reader.py
 
 REM With files (Same with Reader.py, but sorted)
 dir /s /b | sort > manifest_dirs_files.txt
