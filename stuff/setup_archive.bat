53ba79d9e53c43353a9855e219e6b6b800c17954
kittenparry <kittenparry@users.noreply.github.com>
2017-11-10 02:36:18 +0300

    Added 'sports' folder

--- stuff/setup_archive.bat
+++ stuff/setup_archive.bat
@@ -1,2 +1,2 @@
 REM Archive folder setup
-md books documentaries games games\nsfw games\oth games\sfw images movies music music\live_footage music\to_check oth porn porn\animu porn\jav porn\mango porn\pics porn\unorganised porn\vids programs series series\one series\many
+md books documentaries games games\nsfw games\oth games\sfw images movies music music\live_footage music\to_check oth porn porn\animu porn\jav porn\mango porn\pics porn\unorganised porn\vids programs series series\one series\many sports
--- stuff/setup_downloads.bat
+++ stuff/setup_downloads.bat
@@ -1,2 +1,2 @@
 REM Torrent downloads folder setup
-md books documentaries games games\nsfw games\oth games\sfw images movies music music\live_footage music\to_check oth porn porn\unorganised programs series series\one series\many
+md books documentaries games games\nsfw games\oth games\sfw images movies music music\live_footage music\to_check oth porn porn\unorganised programs series series\one series\many sports
