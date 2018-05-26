60308e5389e4953d5dcceffa36871ca732ffb64a
Edvin Boul <kittenparry@users.noreply.github.com>
2018-05-26 23:30:25 +0300

    Update Reader.py
    Output to .html option.
    Visual processing indicator.

--- stuff/Reader.py
+++ stuff/Reader.py
@@ -6,7 +6,7 @@ import sys
 '''
 Usage in command line:
 >Python Reader.py manifest.txt
-Creates a list of files in the current dir and subdirs to 'manifest.txt'
+Creates a list of files in the current dir and subdirs to 'manifest.txt/html'
 which will be placed to the current directory
 Needs the absolute location of Reader.py if it's in a different location
 You can remove sys.argv[1] and get rid of file variable for a static filename
@@ -15,13 +15,21 @@ class Reader():
     def __init__(self, file):
         archive = ''
         for dirname, dirnames, filenames in os.walk(os.getcwd()):
-            print('.', end='')
             #print path to all subdirectories first
             for subdirname in dirnames:
                 archive += os.path.join(dirname, subdirname) + '\n'
+                if file[-4:] == 'html':
+                     archive += '</br>'
             #print path to all filenames
             for filename in filenames:
                 archive += os.path.join(dirname, filename) + '\n'
+                if file[-4:] == 'html':
+                     archive += '</br>'
+                #visual processing indicator
+                l = len(subdirname) % 10
+                s = l * '.'
+                sys.stdout.write("\r%s" % s)
+                sys.stdout.flush()
         try:
             with io.open(str(file), 'w', encoding='utf8') as f:
                 f.write(archive)
