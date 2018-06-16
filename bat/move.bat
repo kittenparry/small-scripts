ce503d05f080fbce50b6b3468a1ca86b79764bd9
Edvin Boul <kittenparry@users.noreply.github.com>
2018-06-16 05:44:02 +0300

    Edit move.bat
    Rename "folder.*" to "f.*", "f.rar" to "f.comp", "f.video" to "f.vid"
    Variables instead of names
    Less repetition
    Added .dmg to "f.comp"
    Added .swf .ods .db .xml .ini .sh .php to "f.oth"

--- bat/move.bat
+++ bat/move.bat
@@ -1,46 +1,29 @@
 REM Creates the folders for my downloads folder
 REM and puts files to their places
 
-MD folder.torrent folder.img folder.webm folder.exe folder.rar folder.video folder.oth
-MOVE *.torrent folder.torrent\
-MOVE *.jpg folder.img\
-MOVE *.png folder.img\
-MOVE *.gif folder.img\
-MOVE *.jpeg folder.img\
-MOVE *.webm folder.webm\
-MOVE *.exe folder.exe\
-MOVE *.msi folder.exe\
-MOVE *.rar folder.rar\
-MOVE *.zip folder.rar\
-MOVE *.7z folder.rar\
-MOVE *.mp4 folder.video\
-MOVE *.avi folder.video\
-MOVE *.flv folder.video\
-MOVE *.ts folder.video\
-MOVE *.mov folder.video\
-MOVE *.wmv folder.video\
-MOVE *.mkv folder.video\
-MOVE *.3gp folder.video\
-MOVE *.m4v folder.video\
-MOVE *.mpg folder.video\
-MOVE *.pdf folder.oth\
-MOVE *.mp3 folder.oth\
-MOVE *.txt folder.oth\
-MOVE *.iso folder.oth\
-MOVE *.docx folder.oth\
-MOVE *.rtf folder.oth\
-MOVE *.svg folder.oth\
-MOVE *.js folder.oth\
-MOVE *.pptx folder.oth\
-MOVE *.doc folder.oth\
-MOVE *.m4a folder.oth\
-MOVE *.html folder.oth\
-MOVE *.css folder.oth\
-MOVE *.ogg folder.oth\
-MOVE *.xlsx folder.oth\
-MOVE *.gz folder.oth\
-MOVE *.azw3 folder.oth\
-MOVE *.mobi folder.oth\
-MOVE *.log folder.oth\
-MOVE *.jar folder.oth\
-MOVE *.scs folder.oth\
+REM Set folder names
+SET t=f.torrent
+SET i=f.img
+SET w=f.webm
+SET e=f.exe
+SET c=f.comp
+SET v=f.vid
+SET o=f.oth
+
+REM Set extensions
+SET te=.torrent
+SET ie=.jpg .png .gif .jpeg
+SET we=.webm
+SET ee=.exe .msi
+SET ce=.rar .zip .7z .dmg
+SET ve=.mp4 .avi .flv .ts .mov .vmw .mkv .3gp .m4v .mpg
+SET oe=.pdf .mp3 .txt .iso .docx .rtf .svg .js .pptx .doc .m4a .html .css .ogg .xlsx .gz .azw3 .mobi .log .jar .scs .swf .ods .db .xml .ini .sh .php
+
+MD %t% %i% %w% %e% %c% %v% %o%
+FOR %%a IN (%te%) DO MOVE *%%a %t%
+FOR %%a IN (%ie%) DO MOVE *%%a %i%
+FOR %%a IN (%we%) DO MOVE *%%a %w%
+FOR %%a IN (%ee%) DO MOVE *%%a %e%
+FOR %%a IN (%ce%) DO MOVE *%%a %c%
+FOR %%a IN (%ve%) DO MOVE *%%a %v%
+FOR %%a IN (%oe%) DO MOVE *%%a %o%
