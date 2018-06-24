676b9052db63405795982e425d221ce3196524fa
Edvin Boul <kittenparry@users.noreply.github.com>
2018-06-24 04:45:09 +0300

    Removed commented out code
    I can always go back to see them on the changelogs

--- js/reddit.com.js
+++ js/reddit.com.js
@@ -10,29 +10,6 @@ function checkKeyPressed(e){
 			case 78:
 				window.location=document.querySelector('a[rel="nofollow next"]').href;
 				break;
-			/* disabled tests below
-			case 82: //r
-				//window.location=document.querySelector('a[class="random choice"]').href;
-				window.location='https://www.reddit.com/r/random/';
-				break;
-			case 84: //t
-				//window.location=document.querySelector('a[class="random choice"]').href;
-				//window.location=$('a:contains("randnsfw")').attr('href');
-				window.location='https://www.reddit.com/r/randnsfw/';
-				break;
-			case 85: //u
-				$('.up:first').trigger('click');
-				break;
-			case 73: //i
-				$('.upmod:first').trigger('click');
-				break;
-			case 49: //1
-				$('.noncollapsed.comment .expand:eq(0)').not('.child .noncollapsed.comment .expand:eq(0)').trigger('click');
-				break;
-			case 50: //2
-				//$('.comment.collapsed .expand:eq(-1)').trigger('click');
-				break;
-			*/
 			default:
 		}
 	}
