eb9e7938476a69eb5ef48b4fdd9d37a1ffe7dc10
Edvin Boul <kittenparry@users.noreply.github.com>
2018-06-25 20:08:18 +0300

    Update del.bat 2
    Doesn't try to delete local .diz .exe .dat files anymore.

--- bat/del.bat
+++ bat/del.bat
@@ -1,6 +1,10 @@
 REM deletes unnecessary files from torrents in %x%:\Archive\porn
 REM DEL /S /F *.nfo *.diz *.exe *.dat
-SET x=:\Archive\porn\*.nfo *.diz *.exe *.dat
+SET x=:\Archive\porn\
 SET dl=D E F G H I J
-FOR %%a IN (%dl%) DO DEL /S /F %%a%x%
+FOR %%a IN (%dl%) DO (
+	%%a:
+	CD %%a%x%
+	DEL /S /F *.nfo *.diz *.exe *.dat
+)
 PAUSE
