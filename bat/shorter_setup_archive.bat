08152cfc3420f25d4a0579ffec8505749d45d0e3
Edvin Boul <kittenparry@users.noreply.github.com>
2018-06-25 20:41:19 +0300

    Created shorter path versions of setup_*.bat files
    I don't know if I would switch to these to save a few letters off of path length.

---
+++ bat/shorter_setup_archive.bat
@@ -0,0 +1,2 @@
+REM Archive folder setup (shorter path)
+MD b d g g\n g\o g\s i m mu mu\lf mu\tc o p p\a p\j p\m p\p p\u p\v pr s s\o s\m sp
---
+++ bat/shorter_setup_downloads.bat
@@ -0,0 +1,2 @@
+REM Torrent downloads folder setup (shorter path)
+MD b d g g\n g\o g\s i m mu mu\lf mu\tc o p p\u pr s s\o s\m sp
