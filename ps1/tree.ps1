35028092ef294ea481c08704bb5e5a9fab3b3d22
Edvin Boul <kittenparry@users.noreply.github.com>
2018-06-26 19:03:12 +0300

    Update tree.ps1 2
    Add functionality to copy output to the dated folders in Google Drive after creation
    Add bunch of variables, descriptive comments

--- ps1/tree.ps1
+++ ps1/tree.ps1
@@ -1,13 +1,39 @@
 #Creates a tree for archive/downloads folders in each drive
+#then copies them to the dated Google Drive folder
+
+#SET paths, drive letters array
 $al =":\Archive\"
 $dl =":\Downloads\"
 $d = @('D', 'E', 'F', 'G', 'H', 'I', 'J')
+#SET cmd, fnames
+$cmd = "TREE /F > "
+$fna = "Archive-"
+$fnd = "Downloads-"
+$fne = "-tree.txt"
+#SET Google Drive path, date
+$g = "F:\Google Drive\Other\Trees\"
+$dt = Get-Date -UFormat "%Y.%m.%d"
+$gdl = $g + $dt
+#COPY to Google Driver function
+FUNCTION ctd($p){
+	IF(Test-Path -Path $gdl){}ELSE{
+		New-Item -ItemType directory -Path $gdl
+	}
+	Copy-Item -Path $p -Destination $gdl
+}
 FOR ($i=0;$i-lt$d.Length;$i++){
-	$lo = ""
+	#SET fnames, commands
+	$fna1 = $fna + $d[$i] + $fne
+	$fnd1 = $fnd + $d[$i] + $fne
+	$cmda = $cmd + $fna1
+	$cmdd = $cmd + $fnd1
+	#SET paths, CD, TREE, COPY to Google Drive
 	$lo = $d[$i] + $al
 	CD $lo
-	powershell -command "tree /f > tree.txt"
+	powershell -command $cmda
+	ctd($lo + $fna1)
 	$lo = $d[$i] + $dl
 	CD $lo
-	powershell -command "tree /f > tree.txt"
+	powershell -command $cmdd
+	ctd($lo + $fnd1)
 }
