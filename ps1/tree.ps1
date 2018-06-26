e3b390bdc5938028840bac1307be8c4917b401a5
Edvin Boul <kittenparry@users.noreply.github.com>
2018-06-26 15:41:19 +0300

    Update tree.ps1
    Turn it into a loop that would go through archive/downloads folders in each drive

--- ps1/tree.ps1
+++ ps1/tree.ps1
@@ -1,1 +1,13 @@
-powershell -command "tree /f > tree.txt"
+#Creates a tree for archive/downloads folders in each drive
+$al =":\Archive\"
+$dl =":\Downloads\"
+$d = @('D', 'E', 'F', 'G', 'H', 'I', 'J')
+FOR ($i=0;$i-lt$d.Length;$i++){
+	$lo = ""
+	$lo = $d[$i] + $al
+	CD $lo
+	powershell -command "tree /f > tree.txt"
+	$lo = $d[$i] + $dl
+	CD $lo
+	powershell -command "tree /f > tree.txt"
+}
