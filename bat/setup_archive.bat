131caa2b02e56d3304ef2d44d8f10338723c2a26
Edvin Boul <kittenparry@users.noreply.github.com>
2018-06-26 19:38:12 +0300

    Remove shorter_setup_*.bat & Update setup_*.bat
    Add functionality to setup folders in different drives

--- bat/setup_archive.bat
+++ bat/setup_archive.bat
@@ -1,6 +1,19 @@
-REM Archive folder setup
+REM Archive folder setup in different drives
+
+SET dl=D E F G H I J
+SET l=:\Archive\
+
 SET g=games
 SET m=music
 SET p=porn
 SET s=series
+
+FOR %%a IN (%dl%) DO (
+	%%a:
+	MD %%a%l%
+	CD %%a%l%
+	CALL :mf
+)
+:mf
 MD books documentaries %g% %g%\nsfw %g%\oth %g%\sfw images movies %m% %m%\live_footage %m%\to_check oth %p% %p%\animu %p%\jav %p%\mango %p%\pics %p%\unorganised %p%\vids programs %s% %s%\one %s%\many sports
+EXIT /B 0
--- bat/setup_downloads.bat
+++ bat/setup_downloads.bat
@@ -1,6 +1,19 @@
-REM Torrent downloads folder setup
+REM Torrent downloads folder setup in different drives
+
+SET dl=D E F G H I J
+SET l=:\Downloads\
+
 SET g=games
 SET m=music
 SET p=porn
 SET s=series
+
+FOR %%a IN (%dl%) DO (
+	%%a:
+	MD %%a%l%
+	CD %%a%l%
+	CALL :mf
+)
+:mf
 MD books documentaries %g% %g%\nsfw %g%\oth %g%\sfw images movies %m% %m%\live_footage %m%\to_check oth %p% %p%\unorganised programs %s% %s%\one %s%\many sports
+EXIT /B 0
