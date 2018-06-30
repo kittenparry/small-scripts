f71a17c54afaec2f04f3c11246c14992ccf51f97
Edvin Boul <kittenparry@users.noreply.github.com>
2018-06-30 03:52:56 +0300

    Update sg_archive.bat
    Added functionality to move folders to the parent directory after zipping and backing up

--- bat/sg_archive.bat
+++ bat/sg_archive.bat
@@ -1,9 +1,16 @@
 REM Zips SuicideGirls folders with appropriate names
-REM and moves them to the Google Drive folder
+REM and moves zips to the Google Drive folder
+REM then moves folders to the parent directory
 
 SET sgp="F:\Google Drive\Photos\Inappropriate\SuicideGirlsdotcom"
 FOR /D %%d IN (*.*) DO "C:\Program Files\7-Zip\7z.exe" a -tzip "%%d.zip" ".\%%d\*"
 MOVE *.zip %sgp%
-REM MOVE folders to parent directory test below
-REM XCOPY *.* .. /exclude:archive.bat
-PAUSE
+FOR /d %%f IN (*.*) DO (
+	IF NOT %%f==sg_archive.bat (
+		CD %%f
+		MD ..\..\%%f\
+		FOR %%i IN (*.*) DO MOVE %%i ..\..\%%f\
+		CD ..
+		RMDIR /S /Q %%f
+	)
+)
