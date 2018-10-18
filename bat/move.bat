ed9d1e7905a3aa4834138a958519446804b9b0c6
Edvin Boul <kittenparry@users.noreply.github.com>
2018-10-18 16:17:10 +0300

    Update move.bat
    Add .bmp to "f.img"
    Add .package .ttf .bk2 .csv to "f.oth"

--- bat/move.bat
+++ bat/move.bat
@@ -18,7 +18,7 @@ SET o=f.oth
 
 REM SET extensions
 SET te=.torrent
-SET ie=.jpg .png .gif .jpeg
+SET ie=.jpg .png .gif .jpeg .bmp
 SET we=.webm
 SET ee=.exe .msi
 SET ce=.rar .zip .7z .dmg
@@ -26,7 +26,7 @@ SET ve=.mp4 .avi .flv .ts .mov .wmv .mkv .3gp .m4v .mpg
 SET oe=.pdf .mp3 .txt .iso .docx .rtf .svg .js .pptx .doc
 SET oe2=.m4a .html .css .ogg .xlsx .gz .azw3 .mobi .log .jar
 SET oe3=.scs .swf .ods .db .xml .ini .sh .php .ogx .save
-SET oe4=.htmlz .h .class .rpa .rpy .sav
+SET oe4=.htmlz .h .class .rpa .rpy .sav .package .ttf .bk2 .csv
 
 MD %t% %i% %w% %e% %c% %v% %o%
 FOR %%a IN (%te%) DO MOVE *%%a %t%
