9535d29e107054aebe098c2b228f4107a8d5cef6
Edvin Boul <kittenparry@users.noreply.github.com>
2018-11-02 10:37:17 +0300

    Rename to robocopy_cr.bat

--- bat/robocopy_call_recordings.bat
+++
@@ -1,4 +0,0 @@
-REM backs up callrecordings folder using ROBOCOPY
-
-ROBOCOPY "G:\fromNewPhone\callrecordings" "F:\Google Drive\fromPC\callrecordings" /MIR /DCOPY:T /XF .nomedia
-PAUSE
---
+++ bat/robocopy_cr.bat
@@ -0,0 +1,4 @@
+REM backs up cr folder using ROBOCOPY
+
+ROBOCOPY "G:\fromNewPhone\cr" "F:\Google Drive\fromPC\cr" /MIR /DCOPY:T /XF .nomedia
+PAUSE
