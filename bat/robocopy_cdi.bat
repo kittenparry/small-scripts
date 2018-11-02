9998db9c16828120e267f16f8ee9ce01e3a30aa5
Edvin Boul <kittenparry@users.noreply.github.com>
2018-11-02 10:03:43 +0300

    Update robocopy_cdi.bat
    Remove F from disks
    Change destination location
    Create a variable for source location

--- bat/robocopy_cdi.bat
+++ bat/robocopy_cdi.bat
@@ -1,12 +1,13 @@
 REM backs up compressed files folder using ROBOCOPY
 
-SET l=D E F G H I J K
-SET p=:\_bup2\c
+SET l=D E G H I J K
+SET p=:\Archive\oth\cdi
+SET lo=F%p%
 FOR %%a IN (%l%) DO (
 	%%a:
 	MD %%a%p%
-	ROBOCOPY "F:\_temp\c" "%%a%p%" /MIR /DCOPY:T
+	ROBOCOPY "%lo%" "%%a%p%" /MIR /DCOPY:T
 )
-ROBOCOPY "F:\_temp\c" "F:\Google Drive\Other\Archived\Compressed\c" /MIR /DCOPY:T
+ROBOCOPY "%lo%" "F:\Google Drive\Other\Archived\Compressed\cdi" /MIR /DCOPY:T
 
 PAUSE
