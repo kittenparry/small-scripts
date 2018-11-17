4b63cf24df52e9391487bfc3935a3998ba8a9b1a
Edvin Boul <kittenparry@users.noreply.github.com>
2018-11-17 19:02:08 +0300

    Create robocopy_workspaces.bat

---
+++ bat/robocopy_workspaces.bat
@@ -0,0 +1,15 @@
+REM backs up workspaces using ROBOCOPY
+
+SET g=F:\Google Drive\Workspaces\
+SET a=/MIR /DCOPY:T
+SET r=ROBOCOPY
+
+%r% "D:\androidstudio" "%g%androidstudio" %a%
+%r% "D:\codeblocksworkspace" "%g%codeblocks" %a%
+%r% "D:\eclipseworkspace" "%g%eclipse" %a%
+%r% "D:\intelliJworkspace" "%g%intelliJ" %a%
+%r% "D:\photoshopworkspace" "%g%photoshop" %a%
+%r% "D:\pycharmworkspace" "%g%pycharm" %a%
+%r% "D:\renpyworkspace" "%g%renpy" %a%
+%r% "D:\webstormworkspace" "%g%webstorm" %a%
+PAUSE
