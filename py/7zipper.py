a2eeabaf01b3f5cd99d65f2cdbde23d74c6f1570
Edvin Boul <kittenparry@users.noreply.github.com>
2018-11-17 06:50:11 +0300

    Update 7zipper.py
    Remove the clutter
    Name actually comes from the current directory
    Password is now entered through a command line argument

--- py/7zipper.py
+++ py/7zipper.py
@@ -1,27 +1,23 @@
 import os
 import subprocess
+import sys
 
 '''
+    Usage in command line after navigating to the desired folder:
+    >7zipper.py password
     Creates archives with all the folders/files in a folder
     Adds password protection, encrypted file names, no compression
     Archives are named after the hexadecimal value of the current directory
     Work in progress.............
     TODO:
-        Pword to cmd argument
         Del files afterwards
 '''
 def seven_zip(pword):
     path_7z = r"C:\Program Files\7-Zip\7z.exe"
-    cwd = os.getcwd()
-    zname = os.path.dirname(os.path.realpath(__file__)).split('\\')[-1]
+    zname = os.getcwd().split('\\')[-1]
     zname_hex = str(zname).encode('utf-8').hex()
-    files1 = [f for f in os.listdir(os.curdir)]
-    files2 = os.listdir(os.curdir)
-    #print(os.listdir(os.curdir))
-    #print(files)
-
     cmd = r'"{}" a "{}" "{}" -p"{}" -mhe=on -mx0'.format(path_7z, zname_hex, ".", pword)
     subprocess.call(cmd, shell=True)
 
-#Swap below with "" for manual password entry
-seven_zip(1)
+if __name__ == '__main__':
+    app = seven_zip(sys.argv[1])
