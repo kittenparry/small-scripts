1589b0f73d422cdcac82a8c856b1f77a0f1e9fca
Edvin Boul <kittenparry@users.noreply.github.com>
2018-11-17 07:01:50 +0300

    Update 7zipper.py 2
    Overall pruning

--- py/7zipper.py
+++ py/7zipper.py
@@ -5,18 +5,17 @@ import sys
 '''
     Usage in command line after navigating to the desired folder:
     >7zipper.py password
-    Creates archives with all the folders/files in a folder
+    Creates an archive with all the data in a folder
     Adds password protection, encrypted file names, no compression
     Archives are named after the hexadecimal value of the current directory
     Work in progress.............
     TODO:
-        Del files afterwards
+        Del files afterwards (as an argumental option)
 '''
 def seven_zip(pword):
     path_7z = r"C:\Program Files\7-Zip\7z.exe"
-    zname = os.getcwd().split('\\')[-1]
-    zname_hex = str(zname).encode('utf-8').hex()
-    cmd = r'"{}" a "{}" "{}" -p"{}" -mhe=on -mx0'.format(path_7z, zname_hex, ".", pword)
+    zname = os.getcwd().split('\\')[-1].encode('utf-8').hex()
+    cmd = r'"{}" a "{}" "{}" -p"{}" -mhe=on -mx0'.format(path_7z, zname, ".", pword)
     subprocess.call(cmd, shell=True)
 
 if __name__ == '__main__':
