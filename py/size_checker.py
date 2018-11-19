c7df2c86f774bfd909067cf8df664bce34de51fa
Edvin Boul <kittenparry@users.noreply.github.com>
2018-11-19 20:48:05 +0300

    Update size_checker.py
    Remove comments, clutter

--- py/size_checker.py
+++ py/size_checker.py
@@ -1,28 +1,21 @@
 import os
-import sys
 
+'''
+    Usage in command line after navigating to the desired folder:
+    >size_checker.py
+    Prints out the number and total size of all files in a folder.
+'''
 def convert_bytes(num):
     for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
         if num < 1024.0:
             return "%3.1f %s" % (num, x)
         num /= 1024.0
-'''
-def file_size(file_path):
-    if os.path.isfile(file_path):
-        file_info = os.stat(file_path)
-        return convert_bytes(file_info.st_size)
-'''
-#file_path = r"J:\jdownloader7\6a646f776e6c6f6164657237.7z"
-#print(file_size(file_path))
-#files = os.listdir(os.curdir)
-files = [f for f in os.listdir(os.curdir) if os.path.isfile(f)]
-fsize = 0
-for file in files:
-    fsize += os.stat(file).st_size
+def file_size():
+    files = [f for f in os.listdir(os.curdir) if os.path.isfile(f)]
+    fsize = 0
+    for file in files:
+        fsize += os.stat(file).st_size
+    print("{} files, {}.".format(len(files), convert_bytes(fsize)))
 
-print(files)
-print(fsize)
-print (convert_bytes(fsize))
-
-#if __name__ == '__main__':
-#    app = seven_zip(sys.argv[1])
+if __name__ == '__main__':
+    app = file_size()
