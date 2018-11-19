dd460818d42aa39b716e129f33bf9708b7586335
Edvin Boul <kittenparry@users.noreply.github.com>
2018-11-19 20:54:03 +0300

    Update 7zipper.py
    Add the functionality of size_checker.py (for files only)

--- py/7zipper.py
+++ py/7zipper.py
@@ -9,14 +9,32 @@ import sys
     Adds password protection, encrypted file names, no compression
     Archives are named after the hexadecimal value of the current directory
     Work in progress.............
+    >7zipper.py -s
+    Displays the number of files and their total size in a folder
     TODO:
-        Del files afterwards (as an argumental option)
+        Del files afterwards (as an argumental option (don't del if blank?))
+            "7zipper.py p d" vs "7zipper.py p" for example?
+        Display the filesize for folders as well
 '''
 def seven_zip(pword):
     path_7z = r"C:\Program Files\7-Zip\7z.exe"
     zname = os.getcwd().split('\\')[-1].encode('utf-8').hex()
     cmd = r'"{}" a "{}" "{}" -p"{}" -mhe=on -mx0'.format(path_7z, zname, ".", pword)
     subprocess.call(cmd, shell=True)
+def convert_bytes(num):
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+def file_size():
+    files = [f for f in os.listdir(os.curdir) if os.path.isfile(f)]
+    fsize = 0
+    for file in files:
+        fsize += os.stat(file).st_size
+    print("{} files, {}.".format(len(files), convert_bytes(fsize)))
 
 if __name__ == '__main__':
-    app = seven_zip(sys.argv[1])
+    if sys.argv[1] == "-s":
+        app = file_size()
+    else:
+        app = seven_zip(sys.argv[1])
