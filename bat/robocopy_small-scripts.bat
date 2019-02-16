c12550e7773f76fe07f2aa375c0820c22cc2e0a9
Edvin Boul <kittenparry@users.noreply.github.com>
2019-02-16 21:43:34 +0300

    Add userscripts folders

--- bat/robocopy_small-scripts.bat
+++ bat/robocopy_small-scripts.bat
@@ -1,4 +1,7 @@
-REM backs up small-scripts folder using ROBOCOPY
+REM backs up scripts folders using ROBOCOPY
+REM TODO: remove repetition
 
 ROBOCOPY "D:\git\small-scripts" "F:\Google Drive\Programs\small-scripts" /MIR /DCOPY:T /XD .git
+ROBOCOPY "D:\git\userscripts" "F:\Google Drive\Programs\userscripts" /MIR /DCOPY:T /XD .git
+ROBOCOPY "D:\git\userscripts-private" "F:\Google Drive\Programs\userscripts-private" /MIR /DCOPY:T /XD .git
 PAUSE
