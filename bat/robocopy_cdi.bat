f93cc092def5e6e59b1ac3515c670c6a97fab918
Edvin Boul <kittenparry@users.noreply.github.com>
2019-04-15 16:58:12 +0300

    Comment out commands so I don't run them again
    These were meant to be used only once, so maybe a simple copy would have done
    Commenting out now to not mistakenly run them in future

--- bat/robocopy_cdi.bat
+++ bat/robocopy_cdi.bat
@@ -6,7 +6,7 @@ SET lo=F%p%
 FOR %%a IN (%l%) DO (
 	%%a:
 	MD %%a%p%
-	ROBOCOPY "%lo%" "%%a%p%" /MIR /DCOPY:T
+	REM ROBOCOPY "%lo%" "%%a%p%" /MIR /DCOPY:T
 )
-ROBOCOPY "%lo%" "F:\Google Drive\Other\Archived\Compressed\636469" /MIR /DCOPY:T
+REM ROBOCOPY "%lo%" "F:\Google Drive\Other\Archived\Compressed\636469" /MIR /DCOPY:T
 PAUSE
