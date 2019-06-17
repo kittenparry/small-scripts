65289ff1f02582d4dbb60ac3c124a17b84adb310
Edvin Boul <kittenparry@users.noreply.github.com>
2019-06-17 03:41:44 +0300

    Less dots for move.bat (#3)
    Also add .py to f.oth

--- bat/move.bat
+++ bat/move.bat
@@ -17,23 +17,23 @@ SET v=f.vid
 SET o=f.oth
 
 REM SET extensions
-SET te=.torrent
-SET ie=.jpg .png .gif .jpeg .bmp .webp
-SET we=.webm
-SET ee=.exe .msi
-SET ce=.rar .zip .7z .dmg
-SET ve=.mp4 .avi .flv .ts .mov .wmv .mkv .3gp .m4v .mpg
-SET oe=.pdf .mp3 .txt .iso .docx .rtf .svg .js .pptx .doc
-SET oe2=.m4a .html .css .ogg .xlsx .gz .azw3 .mobi .log .jar
-SET oe3=.scs .swf .ods .db .xml .ini .sh .php .ogx .save
-SET oe4=.htmlz .h .class .rpa .rpy .sav .package .ttf .bk2 .csv
-SET oe5=.renpy .srt .apk .sdltm .dll 
+SET te=torrent
+SET ie=jpg png gif jpeg bmp webp
+SET we=webm
+SET ee=exe msi
+SET ce=rar zip 7z dmg
+SET ve=mp4 avi flv ts mov wmv mkv 3gp m4v mpg
+SET oe=pdf mp3 txt iso docx rtf svg js pptx doc
+SET oe2=m4a html css ogg xlsx gz azw3 mobi log jar
+SET oe3=scs swf ods db xml ini sh php ogx save
+SET oe4=htmlz h class rpa rpy sav package ttf bk2 csv
+SET oe5=renpy srt apk sdltm dll py
 
 MD %t% %i% %w% %e% %c% %v% %o%
-FOR %%a IN (%te%) DO MOVE *%%a %t%
-FOR %%a IN (%ie%) DO MOVE *%%a %i%
-FOR %%a IN (%we%) DO MOVE *%%a %w%
-FOR %%a IN (%ee%) DO MOVE *%%a %e%
-FOR %%a IN (%ce%) DO MOVE *%%a %c%
-FOR %%a IN (%ve%) DO MOVE *%%a %v%
-FOR %%a IN (%oe%, %oe2%, %oe3%, %oe4%, %oe5%) DO MOVE *%%a %o%
+FOR %%a IN (%te%) DO MOVE *.%%a %t%
+FOR %%a IN (%ie%) DO MOVE *.%%a %i%
+FOR %%a IN (%we%) DO MOVE *.%%a %w%
+FOR %%a IN (%ee%) DO MOVE *.%%a %e%
+FOR %%a IN (%ce%) DO MOVE *.%%a %c%
+FOR %%a IN (%ve%) DO MOVE *.%%a %v%
+FOR %%a IN (%oe%, %oe2%, %oe3%, %oe4%, %oe5%) DO MOVE *.%%a %o%
