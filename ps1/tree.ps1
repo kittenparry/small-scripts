ad96785dd2866b6e23d817be67b0568e77af058c
Edvin Boul <kittenparry@users.noreply.github.com>
2019-10-15 23:40:18 +0300

    Also create a tree of arch/
    Only exists in drive-j but keeps running while outputting errors
    Also godsdamn these are some ugly variable names

--- ps1/tree.ps1
+++ ps1/tree.ps1
@@ -4,11 +4,13 @@
 #SET paths, drive letters array
 $al = ":\Archive\"
 $dl = ":\Downloads\"
+$path_arch = ":\arch\"
 $d = @('D', 'E', 'F', 'G', 'H', 'I', 'J', 'K')
 #SET cmd, fnames
 $cmd = "TREE /F > "
 $fna = "Archive-"
 $fnd = "Downloads-"
+$file_name_arch = "arch-"
 $fne = "-tree.txt"
 #SET Google Drive path, date
 $g = "F:\Google Drive\Other\Trees\"
@@ -25,15 +27,23 @@ FOR ($i=0;$i-lt$d.Length;$i++){
 	#SET fnames, commands
 	$fna1 = $fna + $d[$i] + $fne
 	$fnd1 = $fnd + $d[$i] + $fne
+	$file_name_arch_complete = $file_name_arch + $d[$i] + $fne
 	$cmda = $cmd + $fna1
 	$cmdd = $cmd + $fnd1
+	$cmd_arch = $cmd + $file_name_arch_complete
 	#SET paths, CD, TREE, COPY to Google Drive
 	$lo = $d[$i] + $al
 	CD $lo
 	powershell -command $cmda
 	ctd($lo + $fna1)
+	
 	$lo = $d[$i] + $dl
 	CD $lo
 	powershell -command $cmdd
 	ctd($lo + $fnd1)
+	
+	$lo = $d[$i] + $path_arch
+	CD $lo
+	powershell -command $cmd_arch
+	ctd($lo + $file_name_arch_complete)
 }
