905f9390f4da6f06b3daffaea10f29271e17f912
Edvin Boul <kittenparry@users.noreply.github.com>
2019-10-22 14:03:32 +0300

    Remove variables, different path, verbose, out dir
    Mention of unnecessary variables in https://github.com/kittenparry/dot-files/commit/be01341b3deb82fec864512f22cec63e7e6ba333
    Out dir concatenation might need testing

--- bat/rpa_extractor.bat
+++ bat/rpa_extractor.bat
@@ -1,8 +1,6 @@
 REM RPA Extractor command with a for loop to help the extraction of all files in a path
 REM Usage rpa_extractor.bat path output_folder_name
 
-SET rp="I:\Downloads.old3\f.folder\RPA Extractor for Windows"
-SET rl=I
-%rl%:
-CD %rp%
-FOR %%a IN (%1\*.rpa) DO rpaextractor -x "%%a" -o %2
+F:
+CD "F:\_rpaextractor"
+FOR %%a IN (%1\*.rpa) DO rpaextractor -v -x "%%a" -o out\%2
