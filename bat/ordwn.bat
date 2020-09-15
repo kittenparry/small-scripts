05314ecb009c2a7b448cdadf82581a2363989d8f
Edvin Boul <kittenparry@users.noreply.github.com>
2020-09-15 13:11:52 +0300

    Update with new download paths
    And testing branch of pddcat's file name

--- bat/ordwn.bat
+++ bat/ordwn.bat
@@ -4,9 +4,9 @@ REM https://github.com/kittenparry/pddcat
 REM direct port of its bash counterpart:
 REM https://github.com/kittenparry/dot-files/blob/master/.scripts/ordwn
 
-SET pdd=D:\git\pddcat\pddcat
-SET dwn=I:\Downloads.old3
-SET arch=I:\archxdwn\porn
+SET pdd=D:\git\pddcat\pddcat.py
+SET dwn=D:\dwnWin4
+SET arch=D:\archxdwn\porn
 
 CALL move.bat
 python %pdd% -ts %dwn%\f.img -td %arch%\img
