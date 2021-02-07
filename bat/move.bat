384c81f22f4ee6aa24121ff8b90546a1252b6a8f
Edvin Boul <kittenparry@users.noreply.github.com>
2021-02-07 23:35:10 +0300

    New video, image, other formats

--- bat/move.bat
+++ bat/move.bat
@@ -19,17 +19,17 @@ SET o=f.oth
 
 REM SET extensions
 SET te=torrent
-SET ie=jpg png gif jpeg bmp webp
+SET ie=jpg png gif jpeg bmp webp jfif jpe
 SET we=webm
 SET ee=exe msi
 SET ce=rar zip 7z dmg
-SET ve=mp4 avi flv ts mov wmv mkv 3gp m4v mpg
+SET ve=mp4 avi flv ts mov wmv mkv 3gp m4v mpg 3gpp
 SET oe=pdf mp3 txt iso docx rtf svg js pptx doc
 SET oe2=m4a html css ogg xlsx gz azw3 mobi log jar
 SET oe3=scs swf ods db xml ini sh php ogx save
 SET oe4=htmlz h class rpa rpy sav package ttf bk2 csv
 SET oe5=renpy srt apk sdltm dll py pak ts4script ulf dat
-SET oe6=gpl tar.bz2 ila
+SET oe6=gpl tar.bz2 ila cfg exr hdr tar tif woff2
 
 MD %t% %i% %w% %e% %c% %v% %o%
 FOR %%a IN (%te%) DO MOVE *.%%a %t%
